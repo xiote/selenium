ea6ff7222f1d79e8d945b4382aeb585b58b7355b
xiote <xiote@nate.com>
2020-06-19 23:35:33 +0900

    설치 스크립트 추가

---
+++ src/install-chromedriver.sh
@@ -0,0 +1,5 @@
+# https://www.selenium.dev/documentation/en/getting_started_with_webdriver/third_party_drivers_and_plugins/
+
+wget https://chromedriver.storage.googleapis.com/83.0.4103.39/chromedriver_mac64.zip
+unzip chromedriver_mac64.zip
+rm chromedriver_mac64.zip
---
+++ src/install-selenium-server-standalone.sh
@@ -0,0 +1,3 @@
+# https://www.selenium.dev/downloads/
+wget https://selenium-release.storage.googleapis.com/3.141/selenium-server-standalone-3.141.59.jar
+mv selenium-server-standalone-3.141.59.jar selenium-server-standalone.jar
