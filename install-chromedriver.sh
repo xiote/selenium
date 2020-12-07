a71425ac811a7d73dcfd1242ef7ccebfaf04e22a
xiote <xiote@nate.com>
2020-12-07 11:32:23 +0900

    modified:   install-chromedriver.sh

--- install-chromedriver.sh
+++ install-chromedriver.sh
@@ -1,5 +1,6 @@
 # https://www.selenium.dev/documentation/en/getting_started_with_webdriver/third_party_drivers_and_plugins/
 
-curl -O https://chromedriver.storage.googleapis.com/84.0.4147.30/chromedriver_mac64.zip
+# curl -O https://chromedriver.storage.googleapis.com/84.0.4147.30/chromedriver_mac64.zip
+curl -O https://chromedriver.storage.googleapis.com/87.0.4280.88/chromedriver_mac64.zip
 unzip chromedriver_mac64.zip
 rm chromedriver_mac64.zip
